bfbc176852c16ff328782e36ad14dbe6420b5bd1
Criso <68233846+crisoalmoniga@users.noreply.github.com>
2024-09-01 18:02:04 -0300

    Actualizado a unidad 1

---
+++ Assets/Herir.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Herir : MonoBehaviour
+{
+    // Variables a configurar desde el editor
+    [Header("Configuracion")]
+    [SerializeField] float puntos = 5f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Jugador jugador = collision.gameObject.GetComponent<Jugador>();
+            jugador.ModificarVida(-puntos);
+            Debug.Log(" PUNTOS DE DAÑO REALIZADOS AL JUGADOR " + puntos);
+        }
+    }
+}
