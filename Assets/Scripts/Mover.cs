d7f508478fb522b7eb080bbfe04c708099ddf2e6
Criso <68233846+crisoalmoniga@users.noreply.github.com>
2024-09-01 20:07:10 -0300

    si

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,30 +5,46 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     // Variables a configurar desde el editor
-    [Header("Configuracion")]
+    [Header("Configuración")]
     [SerializeField] float velocidad = 5f;
 
     // Variables de uso interno en el script
     private float moverHorizontal;
     private Vector2 direccion;
 
-    // Variable para referenciar otro componente del objeto
+    // Variable para referenciar otros componentes del objeto
     private Rigidbody2D miRigidbody2D;
+    private Animator miAnimator;
 
-    // Codigo ejecutado cuando el objeto se activa en el nivel
+    // Código ejecutado cuando el objeto se activa en el nivel
     private void OnEnable()
     {
         miRigidbody2D = GetComponent<Rigidbody2D>();
+        miAnimator = GetComponent<Animator>();
     }
 
-    // Codigo ejecutado en cada frame del juego (Intervalo variable)
+    // Código ejecutado en cada frame del juego (intervalo variable)
     private void Update()
     {
         moverHorizontal = Input.GetAxis("Horizontal");
         direccion = new Vector2(moverHorizontal, 0f);
+
+        // Reproducir la animación Jugador_camina si hay movimiento horizontal
+        if (moverHorizontal != 0)
+        {
+            miAnimator.Play("Jugador_camina");
+        }
+        else
+        {
+            miAnimator.Play("Jugador");
+        }
     }
+
+    // Código ejecutado a intervalos fijos para física
     private void FixedUpdate()
     {
         miRigidbody2D.AddForce(direccion * velocidad);
     }
 }
+
+
