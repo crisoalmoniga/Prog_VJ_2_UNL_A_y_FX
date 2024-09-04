50befec44870844ab1c3748c43479a5ab49a33f6
Criso <68233846+crisoalmoniga@users.noreply.github.com>
2024-09-03 23:44:17 -0300

    Entrega lista

--- Assets/Scripts/Saltar.cs
+++ Assets/Scripts/Saltar.cs
@@ -4,45 +4,58 @@ using UnityEngine;
 
 public class Saltar : MonoBehaviour
 {
+
     // Variables a configurar desde el editor
-    [Header("Configuración")]
+    [Header("Configuracion")]
     [SerializeField] private float fuerzaSalto = 5f;
+
+    [SerializeField] private AudioClip jumpSFX;
+    [SerializeField] private AudioClip collisionSFX;
 
     // Variables de uso interno en el script
     private bool puedoSaltar = true;
     private bool saltando = false;
 
-    // Variable para referenciar otros componentes del objeto
+    // Variable para referenciar otro componente del objeto
     private Rigidbody2D miRigidbody2D;
+    private AudioSource miAudioSource;
 
-    // Código ejecutado cuando el objeto se activa en el nivel
+    // Codigo ejecutado cuando el objeto se activa en el nivel
     private void OnEnable()
     {
         miRigidbody2D = GetComponent<Rigidbody2D>();
+        miAudioSource = GetComponent<AudioSource>();
     }
 
-    // Código ejecutado en cada frame del juego (Intervalo variable)
+    // Codigo ejecutado en cada frame del juego (Intervalo variable)
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && puedoSaltar)
         {
             puedoSaltar = false;
-            saltando = true;
+
+            if (miAudioSource.isPlaying) { return; }
+            miAudioSource.PlayOneShot(jumpSFX);
         }
     }
 
     private void FixedUpdate()
     {
-        if (saltando)
+        if (!puedoSaltar && !saltando)
         {
             miRigidbody2D.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
-            saltando = false; // Se asegura de que el impulso solo se aplique una vez
+            saltando = true;
         }
     }
 
-    // Código ejecutado cuando el jugador colisiona con otro objeto
+    // Codigo ejecutado cuando el jugador colisiona con otro objeto
     private void OnCollisionEnter2D(Collision2D collision)
     {
         puedoSaltar = true;
+        saltando = false;
+
+        if (miAudioSource.isPlaying) { return; }
+        miAudioSource.PlayOneShot(collisionSFX);
     }
+
 }
